20e30c0c6b54181b74e6420a7a90640c0fd4dc02
Gilvan <45008443+Gilvan-Araujo@users.noreply.github.com>
2021-05-06 16:17:11 -0300

    Fix accessibility issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/global.scss'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=yes' />
+      </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,10 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="pt-BR">
                 <Head>
                     <meta charSet="UTF-8" />
                     <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-                    <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
                     <meta name='description' content='Description' />
                     <meta name='keywords' content='Keywords' />
 
