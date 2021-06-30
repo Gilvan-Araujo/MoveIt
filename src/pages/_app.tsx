d9c00ea5d42e0f6d015998ec8645e325032a9bfa
Gilvan Araújo <gilvanjunior@protonmail.com>
2021-06-30 10:26:49 -0300

    :sparkles: feat: install chakra ui and setup provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
 import '../styles/fonts.scss'
 import '../styles/global.scss'
+
 import Head from 'next/head'
+
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ChakraProvider>
       <Head>
-        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5,user-scalable=yes' />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5,user-scalable=yes"
+        />
       </Head>
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   )
 }
 
