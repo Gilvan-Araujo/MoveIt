9730a4af1f2df0870c5b88e5cf8239a72c09386b
Gilvan Araújo <gilvanjunior@protonmail.com>
2021-07-02 07:40:18 -0300

    :lipstick: style: migrate completed challenges component

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,14 +1,25 @@
-import { useContext } from 'react'
+import react, { useContext } from 'react'
+
 import { ChallengesContext } from '../contexts/ChallengesContext'
-import styles from '../styles/components/CompletedChallenges.module.scss'
+import { Flex, Text } from '@chakra-ui/react'
 
 export function CompletedChallenges() {
-    const { challengesCompleted } = useContext(ChallengesContext)
+  const { challengesCompleted } = useContext(ChallengesContext)
 
-    return (
-        <div className={styles.completedChallengesContainer}>
-            <span>Desafios completos</span>
-            <span>{challengesCompleted}</span>
-        </div>
-    )
+  return (
+    <Flex
+      align="center"
+      justify="space-between"
+      my="3.5rem"
+      mx={0}
+      pb="1rem"
+      borderBottom="solid"
+      borderBottomWidth="1px"
+      borderBottomColor="gray.500"
+      fontWeight={500}
+    >
+      <Text fontSize="1.25rem">Desafios completos</Text>
+      <Text fontSize="1.5rem">{challengesCompleted}</Text>
+    </Flex>
+  )
 }
