7d83c2b0997f0d1e4e618eadc38c9e6d96ebfdf9
Gilvan Araújo <gilvanjunior@protonmail.com>
2021-07-02 08:09:04 -0300

    :lipstick: style: migrate profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,20 +1,30 @@
-import { useContext } from 'react'
+import React, { useContext } from 'react'
+
+import { Flex, Image, Text } from '@chakra-ui/react'
+
 import { ChallengesContext } from '../contexts/ChallengesContext'
-import styles from '../styles/components/Profile.module.scss'
 
 export function Profile() {
-    const { level } = useContext(ChallengesContext);
+  const { level } = useContext(ChallengesContext)
 
-    return (
-        <div className={styles.profileContainer}>
-            <img src="https://github.com/gilvan-araujo.png" alt="Gilvan Araújo" />
-            <div>
-                <strong>Gilvan Araújo</strong>
-                <p>
-                    <img src="icons/level.svg" alt="Level" />
-                    Level {level}
-                </p>
-            </div>
-        </div>
-    )
+  return (
+    <Flex alignItems="center">
+      <Image
+        src="https://github.com/gilvan-araujo.png"
+        alt="Gilvan Araújo"
+        w="5.5rem"
+        h="5.5rem"
+        borderRadius="full"
+      />
+      <Flex ml="1.5rem" flexDirection="column">
+        <Text fontSize="1.5rem" fontWeight={600} color="gray.100">
+          Gilvan Araújo
+        </Text>
+        <Flex fontSize="1rem" mt="0.5rem" flexDirection="row">
+          <Image src="icons/level.svg" alt="Level" mr="0.5rem" />
+          Level {level}
+        </Flex>
+      </Flex>
+    </Flex>
+  )
 }
